50a4e9e0920a5ed4491a34d218e800808ed4c2ba
Rappu <rafhaanabdulla@gmail.com>
2023-05-11 10:31:53 +0500

    Initial commit

---
+++ app/sum/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function sum() {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+          <h1 className="text-6xl font-bold">
+            Welcome to{" "}
+            <a className="text-blue-600" href="https://nextjs.org">
+              Sum Shit
+            </a>
+          </h1>
+        </main>
+      </div>
+    </div>
+  );
+}
